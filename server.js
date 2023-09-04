8984e79b12a6191f06746334ed8ea205001e0ac9
Katherine Yanan Saung <katysaung@gmail.com>
2023-09-04 17:22:26 -0400

    created 10 routes with 2 templates

--- server.js
+++ server.js
@@ -29,24 +29,76 @@ app.set("view engine", "portal"); // set default view engine to portal
 // it knows to render template.portal b/c we created the app.set views folder and app.set the default to portal
 app.get("/", (req, res) =>{
     res.render("template", {
-        title:  "We the Best",
-        message: "Who!",
+        title:  "We the Best.",
+        message: "Biggest Boss",
         content: "We Taking Over, Major Key Alert, Yall know who it is, All I do is win",
     })
 })
 app.get("/about-me", (req, res) =>{
     res.render("template2", {
-        title:  "Hey",
-        message: "Rick Ross!",
-        content: "The most underated Rapper in the game",
+        title:  "Rick Ross",
+        message: "Rick Ross at NRG Stadium for Texans-Colts game",
+        content: "The most underrated Rapper in the game",
         url: "https://usatodayhss.com/wp-content/uploads/sites/96/2016/12/rick-ross-nfl-indianapolis-colts-houston-texans-590x900.jpg?w=590&h=393&crop=1"
     })
 })
-app.get("/another-one", (req, res) =>{
+app.get("/charities", (req, res) =>{
+    res.render("template1", {
+        title:  "Community",
+        message: "Rick Ross and the Community",
+        content: "Rick Ross Gives Out Free Haircuts",
+    })
+})
+app.get("/homes", (req, res) =>{
+    res.render("template2", {
+        title:  "Rick Ross Homes",
+        message: "Where Rick Ross Lives",
+        content: "Miami's Star Island",
+        url: "https://robbreport.com/wp-content/uploads/2023/08/rickrosstarisland.jpg?w=1000"
+    })
+})
+app.get("/cars", (req, res) =>{
+    res.render("template2", {
+        title:  "Rick Ross Cars",
+        message: "Rick Ross Car Collection",
+        content: "100 Car Collection",
+        url: "https://www.bosshunting.com.au/cdn-cgi/imagedelivery/izM8XxyLg9MD6py1ribxJw/www.bosshunting.com.au/2021/07/Rick-Ross-Car-Collection-No-Drivers-License.jpg/w=1200,h=675"
+    })
+})
+app.get("/events", (req, res) =>{
     res.render("template", {
-        title:  "We the Best",
-        message: "Who!",
-        content: "We Taking Over, Major Key Alert, Yall know who it is, All I do is win",
+        title:  "Events and Tours",
+        message: "All Upcoming Events",
+        content: "Dates and Locations",
+    })
+})
+app.get("/discography", (req, res) =>{
+    res.render("template", {
+        title:  "Discography",
+        message: "Top Hits of Rick Ross",
+        content: "Top Songs and Albums",
+    })
+})
+app.get("/store", (req, res) =>{
+     res.render("template2", {
+         title:  "Rick Ross Store",
+         message: "Merchandise",
+         content: "Hurricanes: A Memoir",
+         url: "https://img.thriftbooks.com/api/images/i/m/D36EB7F66EC25F9823B6FB611DAE0EF10B44B787.jpg"
+     })
+})
+app.get("/fans", (req, res) =>{
+     res.render("template", {
+         title:  "Rick Ross Fan Club",
+         message: "Fan Club",
+         content: "Join Rick Ross Fan Club",
+     })
+})
+app.get("/contact", (req, res) =>{
+    res.render("template", {
+        title:  "Contact",
+        message: "Contact Rick Ross",
+        content: "Contact Form",
     })
 })
 
